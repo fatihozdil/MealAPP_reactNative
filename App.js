0fae7ba7f6795240c78b365ec3e960657833eeb7
Muhammed Fatih OZDIL <muhammedfatihozdil@gmail.com>
2021-08-15 17:41:01 +0300

    adding fonts

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
+
 
 export default function App() {
+
+
+  let [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+   
     </View>
   );
 }
@@ -18,4 +33,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontFamily: 'open-sans-bold',
+  }
 });
