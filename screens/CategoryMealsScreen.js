49b8944da9d7d86fed503a7effa3419885086a05
Muhammed Fatih OZDIL <muhammedfatihozdil@gmail.com>
2021-08-15 18:18:51 +0300

    Navigating between screens

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text,StyleSheet} from 'react-native';
+import {View, Text,StyleSheet, Button} from 'react-native';
 
 const CategoryMealsScreen = props => {
     return(
         <View style={styles.screen}>
-            <Text></Text>
+            <Text>Category meals Screen</Text>
+            <Button title="Go to Meal Detail" onPress={()=>{
+                props.navigation.navigate({routeName: 'MealDetail'})
+            }}/>
         </View>
     )
 }
